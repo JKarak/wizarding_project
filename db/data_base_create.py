fd3751d7ed287ef5096d4eefc30d368a8684749e
Kateriabova <riabovakate@yandex.ru>
2023-04-22 20:19:06 +0300

    после АИ

--- db/data_base_create.py
+++ db/data_base_create.py
@@ -46,7 +46,7 @@ class FavouriteSpells(Base):
     spell_uuid = Column(String(36))
     active = Column(Integer(), unique=False)
     date = Column(String(100), nullable=False)
-    user_rel = relationship("User", back_populates="potions_rel_fav")
+    user_rel = relationship("User", back_populates="spells_rel_fav")
 
 
 class ViewedSpells(Base):
